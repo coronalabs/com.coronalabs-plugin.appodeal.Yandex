311b4328c93c01b3b5fc4a4c1a6fc8a2c62ba6ac
Scott Harrison <scottrules44@gmail.com>
2025-09-19 23:27:46 -0400

    Fixes to Network Framework

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -5,7 +5,7 @@ local metadata =
 		format = 'staticLibrary',
 		staticLibs = { 'APDYandexAdapter', 'AppMetricaAdSupport', 'AppMetricaCore', 'AppMetricaCoreExtension', 'AppMetricaCoreUtils', 'AppMetricaCrashes', 'AppMetricaEncodingUtils', 'AppMetricaFMDB', 'AppMetricaHostState', 'AppMetricaIdentifiers', 'AppMetricaKeychain', 'AppMetricaLibraryAdapter', 'AppMetricaLog', 'AppMetricaLogSwift', 'AppMetricaNetwork', 'AppMetricaPlatform', 'AppMetricaProtobuf', 'AppMetricaProtobufUtils', 'AppMetricaScreenshot', 'AppMetricaStorageUtils', 'AppMetricaSynchronization', 'AppMetricaWebKit' },
 		frameworks = { 'YandexMobileAds', },
-		frameworksOptional = {},
+		frameworksOptional = {"Network"},
 	},
 }
 
--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -5,7 +5,7 @@ local metadata =
 		format = 'staticLibrary',
 		staticLibs = { 'APDYandexAdapter' },
 		frameworks = { 'YandexMobileAds', 'AppMetricaAdSupport', 'AppMetricaCore', 'AppMetricaCoreExtension', 'AppMetricaCoreUtils', 'AppMetricaCrashes', 'AppMetricaEncodingUtils', 'AppMetricaFMDB', 'AppMetricaHostState', 'AppMetricaIdentifiers', 'AppMetricaKeychain', 'AppMetricaLibraryAdapter', 'AppMetricaLog', 'AppMetricaLogSwift', 'AppMetricaNetwork', 'AppMetricaPlatform', 'AppMetricaProtobuf', 'AppMetricaProtobufUtils', 'AppMetricaScreenshot', 'AppMetricaStorageUtils', 'AppMetricaSynchronization', 'AppMetricaWebKit', 'DivKit', 'DivKitBinaryCompatibilityFacade', 'LayoutKit', 'LayoutKitInterface', 'Serialization' },
-		frameworksOptional = {},
+		frameworksOptional = {"Network"},
 	},
 }
 
